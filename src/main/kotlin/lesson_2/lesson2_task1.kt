17aa95ef9bc1df110c2b1f1340b6bf6cffbda07b
k.semionov <k.semionov@avasend.com>
2025-02-03 19:50:23 +0300

    lesson2_task1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package jack.ripper.lesson_2
+
+fun main() {
+
+    var Ivan = 3
+    var Alex = 4
+    var Olya = 3
+    var Jack = 5
+
+    var average= ((Ivan + Alex + Olya + Jack)/4.0)
+
+    print(average)
+
+}
