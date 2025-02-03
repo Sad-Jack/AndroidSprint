b02608a036fddc178306681e06e0b550e35c0e38
k.semionov <k.semionov@avasend.com>
2025-02-03 18:24:18 +0300

    finish

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package jack.ripper.lesson_1
+
+fun main() {
+    val totalOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за вашу покупку! Мы ценим ваш выбор."
+    println("Всего заказов: $totalOrders")
+    println(thankYouMessage)
+
+    var workersCounter: Int = 2000
+//    println("Количеcnво работников: $workersCounter")
+    workersCounter -= 1
+    println("Количеcnво работников: $workersCounter")
+}
