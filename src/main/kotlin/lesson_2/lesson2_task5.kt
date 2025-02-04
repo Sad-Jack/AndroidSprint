51040fe524d9a88df1171d763704560ae128c8ba
k.semionov <k.semionov@avasend.com>
2025-02-04 21:34:31 +0300

    fix

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,11 +1,14 @@
 package jack.ripper.lesson_2
 
+const val PERCENT_DIVISOR = 100.0
+
 fun main() {
-    val initialDeposit = 70000
-    val interestRate = 16.7 / 100
+    val initialDeposit = 70_000
+    val interestRatePercent = 16.7
     val years = 20
 
-    val finalAmount = initialDeposit * Math.pow(1 + interestRate, years.toDouble())
+    val interestRate = (interestRatePercent / PERCENT_DIVISOR) + 1
+    val finalAmount = initialDeposit * Math.pow(interestRate, years.toDouble())
 
     println(String.format("%.3f", finalAmount))
 }
