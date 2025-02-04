571c311ee773de2f447091b5e8f4dd89b35f3ec8
k.semionov <k.semionov@avasend.com>
2025-02-04 18:30:51 +0300

    fix волшебных чисел

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -5,12 +5,14 @@ const val SECONDS_IN_HOUR = 3600
 fun main() {
 
     val totalSeconds = 6480
+    val minuteAfterHour = 60
+    val secondsAfterMinute = 60
 
     val hours = totalSeconds / SECONDS_IN_HOUR
     val remainingSecondsAfterHours = totalSeconds % SECONDS_IN_HOUR
 
-    val minutes = remainingSecondsAfterHours / 60
-    val seconds = remainingSecondsAfterHours % 60
+    val minutes = remainingSecondsAfterHours / minuteAfterHour
+    val seconds = remainingSecondsAfterHours % secondsAfterMinute
 
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println("Время в космосе: [$formattedTime]")
