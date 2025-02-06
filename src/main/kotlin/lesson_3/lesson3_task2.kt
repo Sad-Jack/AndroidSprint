d17f716ce6d8b2eda79f284be052f1cdedb5588e
k.semionov <k.semionov@avasend.com>
2025-02-06 22:53:56 +0300

    Смена фамилии

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package jack.ripper.lesson_3
+
+fun main() {
+    var name = "Татьяна"
+    var firstName = "Андреева"
+    var lastName = "Сергеевна"
+    var years = 20
+
+    println("[$firstName $name $lastName, $years лет]")
+
+    firstName = "Сидорова"
+    years = 22
+
+    println("[$firstName $name $lastName, $years лет]")
+}
