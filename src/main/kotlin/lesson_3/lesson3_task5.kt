4e1a19ce5a4a5ffd990ba2c68a30f236926f49af
k.semionov <k.semionov@avasend.com>
2025-02-07 23:40:02 +0300

    Исправления split

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,12 +3,7 @@ package jack.ripper.lesson_3
 fun main() {
     val moveString = "D2-D4;0"
 
-    val moveParts = moveString.split(";")
-    val moveCoordinates = moveParts[0].split("-")
-
-    val from = moveCoordinates[0]
-    val to = moveCoordinates[1]
-    val moveNumber = moveParts[1].toInt()
+    val (from, to, moveNumber) = moveString.split(";", "-")
 
     println("Откуда: $from")
     println("Куда: $to")
