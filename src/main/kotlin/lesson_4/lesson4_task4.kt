624c8d2a644c57e1a6d0839bef7d6f7d7187fe96
k.semionov <k.semionov@avasend.com>
2025-02-13 21:09:34 +0300

    KS-4-4 План тренировок

---
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -0,0 +1,15 @@
+package jack.ripper.lesson_4
+
+fun main() {
+    val day = 5
+    val isArmsAndAbsDay = (day % 2 == 1)
+    val isLegsAndBackDay = !isArmsAndAbsDay
+
+    println("""
+        Упражнения для рук:   $isArmsAndAbsDay
+        Упражнения для ног:   $isLegsAndBackDay
+        Упражнения для спины: $isLegsAndBackDay
+        Упражнения для пресса:$isArmsAndAbsDay
+        """
+    )
+}
