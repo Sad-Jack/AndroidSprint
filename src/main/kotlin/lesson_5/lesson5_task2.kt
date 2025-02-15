7ffade7d9eae3241d6c8e493c024d4e1ad598867
k.semionov <k.semionov@avasend.com>
2025-02-15 20:00:49 +0300

    KS-5-2 Лотерея

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,32 @@
+package jack.ripper.lesson_5
+
+fun main() {
+
+    val winningNumber1 = 7
+    val winningNumber2 = 21
+
+    println("Введите первое число (от 0 до 42): ")
+    val userNumber1 = readLine()?.toIntOrNull()
+
+    println("Введите второе число (от 0 до 42): ")
+    val userNumber2 = readLine()?.toIntOrNull()
+
+    if (userNumber1 == null || userNumber2 == null || userNumber1 !in 0..42 || userNumber2 !in 0..42) {
+        println("Ошибка ввода! Введите числа от 0 до 42.")
+        return
+    }
+
+    val guessedBoth = (userNumber1 == winningNumber1 && userNumber2 == winningNumber2) ||
+            (userNumber1 == winningNumber2 && userNumber2 == winningNumber1)
+
+    val guessedOne = (userNumber1 == winningNumber1 || userNumber1 == winningNumber2) ||
+            (userNumber2 == winningNumber1 || userNumber2 == winningNumber2)
+
+    when {
+        guessedBoth -> println("Поздравляем! Вы выиграли главный приз!")
+        guessedOne -> println("Вы выиграли утешительный приз!")
+        else -> println("Неудача!")
+    }
+
+    println("Выигрышные числа: $winningNumber1 и $winningNumber2")
+}
