7e336f21372752ba70f07287d69a134e34bd7b98
k.semionov <k.semionov@avasend.com>
2025-02-18 21:41:10 +0300

    fix

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,15 +1,18 @@
 package jack.ripper.lesson_5
 
 const val AGE_OF_MAJORITY = 18
+const val AGE_SEVENTEEN = 17
+const val AGE_SIXTEEN = 16
+
 const val CURRENT_YEAR = 2025
 
 fun main() {
-    val userYearBirth = readLine()!!.toInt()
+    val userYearBirth = readln().toIntOrNull() ?: CURRENT_YEAR
 
     val userAge = CURRENT_YEAR - userYearBirth
 
     val resultText = if (userAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом."
-    else if (userAge == 16 || userAge == 17) "Показать экран с ограниченным контентом."
+    else if (userAge == AGE_SIXTEEN || userAge == AGE_SEVENTEEN) "Показать экран с ограниченным контентом."
     else "Вернуться на главный экран."
 
     println(resultText)
