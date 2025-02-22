d0cf8f83b83653441c1fc651a1e04c6318b5b6a6
k.semionov <k.semionov@avasend.com>
2025-02-23 02:06:23 +0300

    KS-7-2 СМС код подтверждения авторизации

---
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -0,0 +1,14 @@
+package jack.ripper.lesson_7
+
+fun main() {
+    while (true) {
+        val code = (1000..9999).random()
+        println("Ваш код авторизации: $code")
+        print("Введите код авторизации:")
+        val userCode = readln().toInt()
+        if (userCode == code) {
+            println("Добро пожаловать!")
+            break
+        }
+    }
+}
