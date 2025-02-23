578265a881dcf5e53bb64d22363e17a4f1df2f3f
k.semionov <k.semionov@avasend.com>
2025-02-23 19:09:05 +0300

    KS-8-4 Замена в массиве

---
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -0,0 +1,21 @@
+package lesson_8
+
+fun main() {
+    val ingredients = arrayOf("мука", "яйца", "молоко", "сахар", "соль", "масло")
+
+    println("Ингредиенты в рецепте: " + ingredients.joinToString(", "))
+
+    print("Введите ингредиент, который хотите заменить: ")
+    val replaceable = readln().lowercase()
+    if (replaceable in ingredients) {
+        val indexIngredient = ingredients.indexOf(replaceable)
+
+        print("Введите ингредиент, который будет заменять его: ")
+        val replacement = readln().lowercase()
+        ingredients[indexIngredient] = replacement
+
+        println("Ингредиенты в рецепте: " + ingredients.joinToString(", "))
+    } else {
+        println("Такого ингредиента в рецепте нет.")
+    }
+}
