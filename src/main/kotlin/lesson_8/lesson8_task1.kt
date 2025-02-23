2b11262927d86a59d5bcf1b8da4858211d13e620
k.semionov <k.semionov@avasend.com>
2025-02-23 03:18:11 +0300

    KS-8-1 Просмотры рекламы

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -1,4 +1,6 @@
 package jack.ripper.lesson_8
 
-class lesson8_task1 {
+fun main() {
+    val viewsPerDay = intArrayOf(120, 150, 180, 200, 220, 250, 300)
+    println("Общее количество просмотров за неделю: ${viewsPerDay.sum()}")
 }
