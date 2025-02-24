97237c287114efb58738014f3f287b79b897e250
k.semionov <k.semionov@avasend.com>
2025-02-25 01:01:12 +0300

    KS-9-2 Изменяемый список

---
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -0,0 +1,14 @@
+package lesson_9
+
+fun main() {
+    val ingredients = mutableListOf("мука", "яйца", "молоко")
+    println("В рецепте есть базовые ингредиенты: " + ingredients.joinToString(", "))
+
+    print("Желаете добавить еще? ")
+    val isWant = readln().lowercase() == "да"
+    if (isWant) {
+        print("Какой ингредиент вы хотите добавить? ")
+        ingredients.add(readln())
+        print("Теперь в рецепте есть следующие ингредиенты: " + ingredients.joinToString(", "))
+    }
+}
