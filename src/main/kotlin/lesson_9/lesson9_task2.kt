5c8633fb8c4f8da89d10eee9ee5acec07da58009
k.semionov <k.semionov@avasend.com>
2025-02-25 00:47:26 +0300

    KS-9-2 Добавление нового ингредиента

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -3,7 +3,6 @@ package lesson_9
 fun main() {
     val ingredients = mutableListOf("мука", "яйца", "молоко")
     println("В рецепте есть базовые ингредиенты: " + ingredients.joinToString(", "))
-
     print("Желаете добавить еще? ")
     val isWant = readln().lowercase() == "да"
     if (isWant) {
