e4902def0ed00afc6af4f6d67c2726f3808081e7
k.semionov <k.semionov@avasend.com>
2025-02-26 01:39:53 +0300

    KS-10-1 Игра в кости

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -1,4 +1,26 @@
 package lesson_10
 
-class lesson10_task1 {
+fun main() {
+    while (true) {
+        println("Для броска кубика отправте 1, для выхода что-то что не 1")
+        val input = readln().toIntOrNull()
+
+        if (input == 1) {
+            Thread.sleep(1000)
+            val computerNumber = rollDice()
+            println("ИИ бросил кубик на: $computerNumber")
+            Thread.sleep(1000)
+            val playerNumber = rollDice()
+            println("Вы бросили кубик на: $playerNumber")
+            println(chooseWinner(computerNumber, playerNumber))
+        } else return
+    }
+}
+
+fun rollDice() = (1..6).random()
+
+fun chooseWinner(computerNumber: Int, playerNumber: Int) = when {
+    computerNumber > playerNumber -> "Победила машина"
+    computerNumber < playerNumber -> "Победило человечество"
+    else -> "Победила дружба"
 }
