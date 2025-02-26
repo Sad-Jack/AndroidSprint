1817a8f39d9aa7b2220ff19ce80b236007e0157f
k.semionov <k.semionov@avasend.com>
2025-02-27 01:06:06 +0300

    KS-11-1 Создания класса пользователя

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,4 +1,25 @@
 package lesson_11
 
-class lesson11_task1 {
+class User(
+    private val token: String,
+    private val login: String,
+    private val password: String,
+    private val email: String
+) {
+    override fun toString(): String {
+        return """
+            |token = '$token',
+            |login = '$login',
+            |password = '$password',
+            |email = '$email'
+        """.trimMargin()
+    }
+}
+
+fun main() {
+    val user1 = User("sd1esd1213", "Jack", "Las_123", "ema@il.ru")
+    val user2 = User("adss12asd3", "Kit", "CRW_123", "em@il.com")
+
+    println(user1)
+    println(user2)
 }
