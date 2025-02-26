240065c79bb82df1f392cfaa7a28b66010f2299a
k.semionov <k.semionov@avasend.com>
2025-02-27 00:45:23 +0300

    fix

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -4,9 +4,10 @@ const val LOGIN = "login"
 const val PASSWORD = "password"
 
 val userBasket = arrayOf("Ноутбук", "Смартфон", "Наушники")
+val chars = (('0'..'9') + ('a'..'z') + ('A'..'Z')).toList()
 
 fun main() {
-    val token: String? = generateToken(authUser())
+    val token: String? = authUser()
     val basket = getBasket(token)
     if (basket != null) {
         println("Ваша корзина: ${basket.joinToString(", ")}")
@@ -15,27 +16,20 @@ fun main() {
     }
 }
 
-fun generateToken(isAuth: Boolean): String? {
-    if (isAuth) {
-        val digits = ('0'..'9')
-        val lowerCase = ('a'..'z')
-        val upperCase = ('A'..'Z')
-        val chars = (digits + lowerCase + upperCase).toList()
-        return (1..32).map { chars.random() }.joinToString("")
-    }
-    return null
-}
-
-fun verificationData(login: String, password: String): Boolean {
-    return (login == LOGIN && password == PASSWORD)
-}
-
-fun authUser(): Boolean {
+fun authUser(): String? {
     print("Ввдите логин: ")
     val login = readln()
     print("Ввдите пароль: ")
     val password = readln()
-    return verificationData(login, password)
+    return if (verifyCredentials(login, password)) generateToken() else null
+}
+
+fun generateToken(): String {
+    return (1..32).map { chars.random() }.joinToString("")
+}
+
+fun verifyCredentials(login: String, password: String): Boolean {
+    return login == LOGIN && password == PASSWORD
 }
 
 fun getBasket(token: String?): Array<String>? {
