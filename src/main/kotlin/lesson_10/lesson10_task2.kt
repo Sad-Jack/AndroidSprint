c98c4b51672a9a5bd363a3a2cb6342b1d5f961cc
k.semionov <k.semionov@avasend.com>
2025-02-27 15:17:28 +0300

    fix

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,16 +1,18 @@
 package lesson_10
 
+const val CREDENTIAL_LENGTH = 4
+
 fun main() {
     println("Зарегистрируйтесь в приложении")
     print("Логин:")
     val inputLogin = readln()
     print("Пароль:")
     val inputPassword = readln()
-    if (isInvalid(inputLogin) || isInvalid(inputPassword)){
+    if (checkValid(inputLogin) || checkValid(inputPassword)){
         println("Логин или пароль недостаточно длинные")
         return
     }
     println("Авторизация прошла успешно")
 }
 
-fun isInvalid(input: String): Boolean = input.length < 4
+fun checkValid(input: String): Boolean = input.length < CREDENTIAL_LENGTH
