6c7b8df5e1564e7b7d4b4873ff074da39f523678
k.semionov <k.semionov@avasend.com>
2025-02-27 15:20:34 +0300

    fix

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,5 +1,8 @@
 package lesson_10
 
+val digits = ('0'..'9')
+val specialCharacters = ("!\"#$%&'()*+,-./")
+
 fun main() {
     print("Введите длину пароля: ")
     val passwordLength = readln().toIntOrNull() ?: return
@@ -8,11 +11,8 @@ fun main() {
 }
 
 fun generatePassword(length: Int): String {
-    val digits = ('0'..'9')
-    val specialCharacters = ("!\"#$%&'()*+,-./")
-    val password = (0 until length).joinToString("") {
+    return (0 until length).joinToString("") {
         if (it % 2 == 0) digits.random().toString()
         else specialCharacters.random().toString()
     }
-    return password
 }
