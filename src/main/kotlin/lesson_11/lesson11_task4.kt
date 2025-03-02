6443844f1687760d5fe19c22c9ae62abbddd01b4
k.semionov <k.semionov@avasend.com>
2025-03-03 00:25:30 +0300

    update

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,11 +1,14 @@
 package lesson_11
 
 data class RecipeCategory(
+    val id: Int,
     val name: String,
-    var picture: String,
+    var image: String,
+    val description: String,
 )
 
 data class Ingredient(
+    val id: Int,
     val name: String,
     val quantity: Double,
     val unit: String,
@@ -13,7 +16,7 @@ data class Ingredient(
 
 data class Recipe(
     val id: Int, // Для добавления в избранное
-    var picture: String,
+    var image: String,
     val title: String,
     val description: String,
     val ingredients: List<Ingredient>,
