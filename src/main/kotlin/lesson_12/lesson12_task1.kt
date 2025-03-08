93af7585eecfdef9d03158dc670adb7e3865ba63
k.semionov <k.semionov@avasend.com>
2025-03-08 14:42:22 +0300

    KS-12-1 Погода

--- src/main/kotlin/lesson_12/lesson12_task1.kt
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -1,4 +1,29 @@
 package lesson_12
 
-class lesson12_task1 {
+class WeatherDay {
+    var dayTemperature: Int = 0
+    var nightTemperature: Int = 0
+    var hasPrecipitation: Boolean = false
+
+    fun printWeather() {
+        println("Дневная температура: $dayTemperature°C")
+        println("Ночная температура: $nightTemperature°C")
+        println("Осадки: ${if (hasPrecipitation) "Да" else "Нет"}")
+        println("---------------")
+    }
+}
+
+fun main() {
+    val weather1 = WeatherDay()
+    weather1.dayTemperature = 25
+    weather1.nightTemperature = 15
+    weather1.hasPrecipitation = true
+
+    val weather2 = WeatherDay()
+    weather2.dayTemperature = 30
+    weather2.nightTemperature = 20
+    weather2.hasPrecipitation = false
+
+    weather1.printWeather()
+    weather2.printWeather()
 }
