e03ef33720a2378e80c2369481d93f3f8e37ba3f
k.semionov <k.semionov@avasend.com>
2025-03-10 22:28:31 +0300

    fix

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,13 +1,8 @@
 package lesson_12
 
 class KelvinWeather(dayTempKelvin: Int, nightTempKelvin: Int, val hasPrecipitation: Boolean) {
-    val dayTemperature: Int
-    val nightTemperature: Int
-
-    init {
-        dayTemperature = dayTempKelvin - 273
-        nightTemperature = nightTempKelvin - 273
-    }
+    val dayTemperature: Int = dayTempKelvin - 273
+    val nightTemperature: Int = nightTempKelvin - 273
 
     fun printWeather() {
         println("Дневная температура: $dayTemperature°C")
